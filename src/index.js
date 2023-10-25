6f0773472740acbec6b05313b141b1c85011cb3a
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-10-25 06:19:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-router-test">
       <App />
     </BrowserRouter>
   </React.StrictMode>
